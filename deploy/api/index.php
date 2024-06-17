24b13256fe98c3444e0a18f46612f9fb841eb95b
Derya AY <derya.ay26@hotmail.com>
2024-06-17 20:32:56 +0200

    projet

--- deploy/api/index.php
+++ deploy/api/index.php
@@ -2,7 +2,8 @@
 
 	use Slim\Factory\AppFactory;
 
-	require __DIR__ . '/./vendor/autoload.php';
+	require __DIR__ . '/../vendor/autoload.php';
+	require_once __DIR__ . '/../bootstrap.php';
 	
 	$app = AppFactory::create();
 
